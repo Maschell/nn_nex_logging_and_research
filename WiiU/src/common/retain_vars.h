0293676f3f191edc4ff2922b48a6c58856c4638d
Maschell <Maschell@gmx.de>
2017-05-17 19:30:33 +0200

    Moved WiiU tool in a subfolder

---
+++ WiiU/src/common/retain_vars.h
@@ -0,0 +1,7 @@
+#ifndef RETAINS_VARS_H_
+#define RETAINS_VARS_H_
+#include <gctypes.h>
+extern volatile u8 encryptionDirection;
+extern volatile u8 logNSSLRead;
+extern volatile u8 logNSSLWrite;
+#endif // RETAINS_VARS_H_
---
+++ WiiU/src/utils/utils.h
@@ -0,0 +1,14 @@
+#ifndef __UTILS_UTILS_H_
+#define __UTILS_UTILS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void bin_to_strhex(unsigned char *bin, unsigned int binsz, char **result);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif//__UTILS_UTILS_H_
